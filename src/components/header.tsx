33128b565db8ba674a9d747adcaf4e781e0ed30a
mateushr23 <mateushr23@gmail.com>
2025-01-23 14:05:24 -0300

    feat: header responsivity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,12 @@ import logo from "../assets/images/core-fecap-logo.png"
 import whatsapp from "../assets/images/whatsapp.png"
 import { Link } from "react-router-dom"
 import { whatsappConfig } from "../assets/whatsapp-config"
+import { useState } from "react"
+import { List } from "@phosphor-icons/react"
 
 export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   const handleWhatsappRedirect = (): void => {
     const phoneNumber = whatsappConfig.phoneNumber.replace(/\D/g, "") // Remove caracteres não numéricos
     const message = encodeURIComponent(
@@ -18,43 +22,66 @@ export function Header() {
 
   return (
     <div className="flex flex-col">
-      <div className="bg-blue300 h-[85px] flex justify-between items-center text-white px-32">
-        <div className="flex gap-4 items-center">
-          <p className="text-sm">Siga nossas redes sociais</p>
+      <div className="bg-blue300 h-[85px] flex justify-between items-center text-white px-8 lg:px-10 xl:px-20 ">
+        <div className="text-xs md:text-sm flex gap-2 items-center">
+          <p className=" flex-1">Siga nossas redes sociais</p>
           <div className="flex gap-1">
             <a
               href="https://www.facebook.com/coreescoladenegocios"
               target="_blank"
             >
-              <img src={facebook} alt="Ícone do Facebook" />
+              <img
+                className="w-5 md:w-auto"
+                src={facebook}
+                alt="Ícone do Facebook"
+              />
             </a>
             <a
               href="https://www.instagram.com/coreescoladenegocios/"
               target="_blank"
             >
-              <img src={instagram} alt="Ícone do Instagram" />
+              <img
+                className="w-5 md:w-auto"
+                src={instagram}
+                alt="Ícone do Instagram"
+              />
             </a>
             <a
               href="https://www.linkedin.com/company/core-escola-de-neg%C3%B3cios/"
               target="_blank"
             >
-              <img src={linkedin} alt="Ícone do LinkedIn" />
+              <img
+                className="w-5 md:w-auto"
+                src={linkedin}
+                alt="Ícone do LinkedIn"
+              />
             </a>
           </div>
         </div>
-        <p>
+        <p className="relative left-4 md:left-4 text-xs md:text-base text-right ">
           Entre em contato
           <span className="font-bold"> contato@corenegocios.com.br</span>
         </p>
       </div>
-      <div className="flex px-32 h-[100px] justify-between items-center bg-white text-black">
-        <img src={logo} alt="Logo da Core e da Fecap" />
-        <div className="flex gap-10 text-blue300 font-semibold text-sm">
+      <div className="flex h-[100px] items-center bg-white text-black px-8 lg:px-10 xl:px-20 justify-between">
+        {/* Logo */}
+        <img
+          className="w-25 lg:w-auto xl:w-auto"
+          src={logo}
+          alt="Logo da Core e da Fecap"
+        />
+
+        {/* Menu (centralizado em telas maiores) */}
+        <div className="hidden lg:flex gap-6 text-blue300 font-semibold text-sm">
           <Link to={"/"} onClick={() => window.scrollTo(0, 0)}>
             HOME
           </Link>
-          <Link to={"/curta-duracao"} onClick={() => window.scrollTo(0, 0)}>
-            CURTA DURAÇÃO
+          <Link
+            className="whitespace-nowrap"
+            to={"/curta-duracao"}
+            onClick={() => window.scrollTo(0, 0)}
+          >
+            CURTA-DURAÇÃO
           </Link>
           <Link to={"/mba"} onClick={() => window.scrollTo(0, 0)}>
             MBA
@@ -62,18 +89,61 @@ export function Header() {
           <Link to={"/tecnicos"} onClick={() => window.scrollTo(0, 0)}>
             TÉCNICOS
           </Link>
+          <Link to={"/incompany"} onClick={() => window.scrollTo(0, 0)}>
+            IN-COMPANY
+          </Link>
           <Link to={"/institucional"} onClick={() => window.scrollTo(0, 0)}>
             INSTITUCIONAL
           </Link>
         </div>
-        <div className="flex gap-4">
+
+        {/* Botões Menu Hambúrguer e WhatsApp */}
+        <div className="flex items-center gap-4">
+          {/* Menu Hamburguer */}
+          <div className="relative lg:hidden">
+            <button
+              className="text-black focus:outline-none"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              <List size={28} />
+            </button>
+            <div
+              className={`${
+                isMenuOpen ? "flex" : "hidden"
+              } absolute right-0 top-full px-10 gap-4 py-4 flex-col items-center bg-white100 shadow-lg text-blue300 font-semibold text-sm z-40`}
+            >
+              <Link to={"/"} onClick={() => window.scrollTo(0, 0)}>
+                HOME
+              </Link>
+              <Link
+                className="whitespace-nowrap"
+                to={"/curta-duracao"}
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                CURTA-DURAÇÃO
+              </Link>
+              <Link to={"/mba"} onClick={() => window.scrollTo(0, 0)}>
+                MBA
+              </Link>
+              <Link to={"/tecnicos"} onClick={() => window.scrollTo(0, 0)}>
+                TÉCNICOS
+              </Link>
+              <Link to={"/incompany"} onClick={() => window.scrollTo(0, 0)}>
+                IN-COMPANY
+              </Link>
+              <Link to={"/institucional"} onClick={() => window.scrollTo(0, 0)}>
+                INSTITUCIONAL
+              </Link>
+            </div>
+          </div>
+
+          {/* Botão WhatsApp */}
           <button
             onClick={handleWhatsappRedirect}
-            className="flex h-[50px] gap-1 border-2 rounded-full border-green200 text-sm 
-          items-center p-4 font-semibold text-green200"
+            className="flex h-[50px] gap-1 border-2 rounded-full border-green200 text-sm items-center p-4 font-semibold text-green200"
           >
             <img src={whatsapp} alt="Ícone do WhatsApp" />
-            FALE CONOSCO
+            <span className="hidden xl:inline">FALE CONOSCO</span>
           </button>
         </div>
       </div>
